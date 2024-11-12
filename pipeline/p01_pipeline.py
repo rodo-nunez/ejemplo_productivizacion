0821506edbaf458c78fb6edb0ae18fd26bb91b0a
Rodo <rodo.nunez.u@gmail.com>
2024-11-12 04:15:51 -0300

    agregamos logging para pipeline

--- pipeline/p01_pipeline.py
+++ pipeline/p01_pipeline.py
@@ -4,6 +4,10 @@ import os, sys
 import argparse
 sys.path.append(os.getcwd()) # Esto es para agregar al path la ruta de ejecución actual y poder importar respecto a la ruta del proyecto, desde donde se debe ejecutar el código
 import params as params
+import logging
+
+# Configuracion del archivo de log ---------------------------------------- 
+logging.basicConfig(filename='files/modeling_output/logs/p01_pipeline_entrenamiento.log', level=logging.INFO, format='%(asctime)s - %(message)s', filemode='w')
 
 # Argumentos por linea de comandos ---------------------------------------- 
 
@@ -28,13 +32,22 @@ print(f"---------------------------------- \nComenzando proceso de entrenamiento
 
 # Preproceso ---------------------------------------- 
 
+logging.info("Ejecutando a01")
 os.system(f"python{extension_binarios} preprocessing/a01_preproceso_general.py")
+logging.info("Ejecutando a02")
 os.system(f"python{extension_binarios} preprocessing/a02_feature_engineering.py")
+logging.info("Ejecutando a03")
 os.system(f"python{extension_binarios} preprocessing/a03_division_train_test.py")
+logging.info("Ejecutando a04")
 os.system(f"python{extension_binarios} preprocessing/a04_preproceso_post_division_train_test.py")
+logging.info("Ejecutando a05")
 os.system(f"python{extension_binarios} preprocessing/a05_preproceso_dependiente_del_modelo.py")
+logging.info("Ejecutando a06")
 os.system(f"python{extension_binarios} preprocessing/a06_pipeline_y_bootstraping_para_comparar_modelos.py")
 
 # Modelo ---------------------------------------- 
 
+logging.info("Ejecutando b01")
 os.system(f"python{extension_binarios} models/b01_entrenamiento_xgboost.py")
+
+logging.info("Terminado p01")
