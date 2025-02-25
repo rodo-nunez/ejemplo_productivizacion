286a04be2ba72e84aa48f6a3e06e4ce2c2d1bfe5
Rodo <rodo.nunez.u@gmail.com>
2025-02-25 00:37:00 -0300

    actualizamos comandos importantes de docker y creamos un archivo que ejecutaremos en el entrypoint

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,3 +1,3 @@
-docker build -t caso_ejemplo_telecom:0.1.1 .
-docker run --network=host -i --name caso_ejemplo_telecom caso_ejemplo_telecom:0.1.1
+docker build -t caso_ejemplo_telecom:0.2.2 .
+docker run --network=host -i --name caso_ejemplo_telecom caso_ejemplo_telecom:0.2.2
 
